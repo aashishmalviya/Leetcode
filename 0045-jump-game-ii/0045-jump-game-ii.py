33d30f0419cdd5b595da9c362b72d52c3c8cfa82
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-10-24 02:56:36 +0530

    Time: 121 ms (54.80%), Space: 17.3 MB (83.89%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,10 +1,27 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        l = r = totalJumps = 0
-        while (r < len(nums) - 1):
-            totalJumps += 1
-            longestJump = max([i + nums[i] for i in range(l,r+1)])
-            l, r = r+1, longestJump
+        maxJumpFromHere = 0
+        totalJumps = 0
+        prevJump = 0
+        
+        for i in range(0, len(nums) - 1):
+            maxJumpFromHere = max(maxJumpFromHere, i + nums[i])
             
+            if(i == prevJump):
+                totalJumps += 1
+                prevJump = maxJumpFromHere
+                
         return totalJumps
+            
+
+
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         l = r = totalJumps = 0
+#         while (r < len(nums) - 1):
+#             totalJumps += 1
+#             longestJump = max([i + nums[i] for i in range(l,r+1)])
+#             l, r = r+1, longestJump
+            
+#         return totalJumps
             
