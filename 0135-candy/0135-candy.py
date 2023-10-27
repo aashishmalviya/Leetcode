c7337226253b5a7d97f370bd5347a2a6cccf4a1b
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-10-28 03:53:32 +0530

    Time: 137 ms (82.47%), Space: 19.4 MB (32.43%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,30 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        totalCandies = len(ratings)
+        
+        i = 1
+        
+        while i < len(ratings):
+            if ratings[i] == ratings[i-1]:
+                i += 1
+                continue
+            
+            peak = 0
+            while i < len(ratings) and ratings[i] > ratings[i-1]:
+                peak += 1
+                totalCandies += peak
+                i += 1
+            
+            if i == len(ratings):
+                return totalCandies
+            
+            valley = 0
+            while i < len(ratings) and ratings[i] < ratings[i-1]:
+                valley += 1
+                totalCandies += valley
+                i += 1
+            
+            totalCandies -= min(peak, valley)
+            
+        return totalCandies
