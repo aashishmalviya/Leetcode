f55665125ba1d9f1b99ab927e50a6a0da7c1f77e
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-10-30 03:15:33 +0530

    Time: 35 ms (91.17%), Space: 16.2 MB (75.20%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def digSquareSum(self, n:int) -> int:
+        finalSum = 0
+        while n > 0:
+            dig = n % 10
+            finalSum += dig * dig
+            n = n//10
+            
+        return finalSum
+    
+    def isHappy(self, n: int) -> bool:
+        slow = fast = n
+        
+        slow = self.digSquareSum(slow)
+        fast = self.digSquareSum(self.digSquareSum(fast))
+
+        while slow!=1 and slow!=fast:
+            slow = self.digSquareSum(slow)
+            fast = self.digSquareSum(self.digSquareSum(fast))
+            
+        return fast == 1
