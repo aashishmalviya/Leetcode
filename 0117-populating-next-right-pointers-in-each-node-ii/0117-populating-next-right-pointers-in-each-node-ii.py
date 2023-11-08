6dc2f7424f83c3b0c4171cf573e05527960b437f
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-11-08 20:25:35 +0530

    Time: 37 ms (99.55%), Space: 17.5 MB (91.80%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -8,27 +8,59 @@ class Node:
         self.next = next
 """
 
+# Solution using deque
+
+# class Solution:
+#     def connect(self, root: 'Node') -> 'Node':
+#         if not root:
+#             return None
+            
+#         q = deque([root])
+        
+#         while q:
+#             prev = None
+            
+#             for _ in range(len(q)):
+#                 currentNode = q.popleft()
+                
+#                 if prev:
+#                     prev.next = currentNode
+                    
+#                 if currentNode.left:
+#                     q.append(currentNode.left)
+#                 if currentNode.right:
+#                     q.append(currentNode.right)
+                    
+#                 prev = currentNode
+            
+#         return root 
+
+#Solution using O(1) space:
+# https://www.youtube.com/watch?v=UbhdyQ3albE
+
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
+        
         if not root:
             return None
-            
-        q = deque([root])
         
-        while q:
-            prev = None
+        current = root
+        
+        while current:
             
-            for _ in range(len(q)):
-                currentNode = q.popleft()
-                
-                if prev:
-                    prev.next = currentNode
-                    
-                if currentNode.left:
-                    q.append(currentNode.left)
-                if currentNode.right:
-                    q.append(currentNode.right)
-                    
-                prev = currentNode
+            dummyNode = prev = Node(0)
             
-        return root 
+            while current:
+                if current.left:
+                    prev.next = current.left
+                    prev = prev.next
+
+                if current.right:
+                    prev.next = current.right
+                    prev = prev.next
+
+                current = current.next
+            
+            current = dummyNode.next
+            
+        return root
