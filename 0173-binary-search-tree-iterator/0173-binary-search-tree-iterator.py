3aed43d28ea4202749155a06e542fe094ef0360e
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-11-09 17:05:28 +0530

    Time: 78 ms (13.82%), Space: 22.9 MB (11.01%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -4,36 +4,64 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+#Morris inorder traversal, TC: O(N), SC: O(1)
+#Ref: https://www.youtube.com/watch?v=wGXB9OWhPTg
+# class BSTIterator:
+
+#     def __init__(self, root: Optional[TreeNode]):
+#         self.currentNode = root
+
+#     def next(self) -> int:
+#         while self.currentNode:
+#             if not self.currentNode.left:
+#                 val = self.currentNode.val
+#                 self.currentNode = self.currentNode.right
+#                 return val
+
+#             else:
+#                 pred = self.currentNode.left
+
+#                 while pred.right and pred.right != self.currentNode:
+#                     pred = pred.right
+
+#                 if pred.right == self.currentNode: #pred right is pointing to currentNode, link already exists
+#                     pred.right = None
+#                     val = self.currentNode.val
+#                     self.currentNode = self.currentNode.right 
+#                     return val
+#                 else:
+#                     pred.right = self.currentNode # link pred right to currentNode
+#                     self.currentNode = self.currentNode.left
+
+
+#     def hasNext(self) -> bool:
+#         return self.currentNode is not None
+
+
+#Ref : https://www.youtube.com/watch?v=D2jMcmxU4bs
+#TC : Avg O(1), SC : O(H)
+
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.currentNode = root
+        self.stack = []
+        self.curr = root
+        self.pushLeftChilds(root)
+    
+    def pushLeftChilds(self, node : TreeNode):
+        while node:
+            self.stack.append(node)
+            node = node.left
 
     def next(self) -> int:
-        while self.currentNode:
-            if not self.currentNode.left:
-                val = self.currentNode.val
-                self.currentNode = self.currentNode.right
-                return val
-
-            else:
-                pred = self.currentNode.left
-
-                while pred.right and pred.right != self.currentNode:
-                    pred = pred.right
-
-                if pred.right == self.currentNode: #pred right is pointing to currentNode, link already exists
-                    pred.right = None
-                    val = self.currentNode.val
-                    self.currentNode = self.currentNode.right 
-                    return val
-                else:
-                    pred.right = self.currentNode # link pred right to currentNode
-                    self.currentNode = self.currentNode.left
-
+        node = self.stack.pop()
+        self.pushLeftChilds(node.right)
+        return node.val
 
     def hasNext(self) -> bool:
-        return self.currentNode is not None
+        return len(self.stack) > 0 
 
 
 # Your BSTIterator object will be instantiated and called as such:
