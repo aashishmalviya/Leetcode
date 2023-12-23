24cffc3ba664fdec9fb6ef3784b247a3c4f212e4
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-23 08:10:56 +0530

    Time: 43 ms (96.14%), Space: 20.5 MB (41.68%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,42 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-
-# class Solution:
-#     smallNodeCounter = 0
-#     ansFound = False
-#     ans = None
-    
-#     def solver(self, root, k):
-#         if root == None or self.ansFound == True:
-#             return
-        
-#         self.solver(root.left, k)
-        
-#         self.smallNodeCounter += 1
-        
-#         if self.smallNodeCounter == k:
-#             self.ansFound = True
-#             self.ans = root.val
-#             return
-        
-#         self.solver(root.right, k)
-        
-#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-#         self.solver(root, k)
-#         return self.ans
-
-
 class Solution:
+    def __init__(self):
+        self.result = 0
+        self.node_count = 0
+        
+    def helper(self, node: TreeNode, k: int):
+        if not node or self.node_count>k:
+            return
+        
+        self.helper(node.left, k)
+        
+        self.node_count += 1
+        
+        if self.node_count == k:
+            self.result = node.val
+            return
+        
+        self.helper(node.right, k)
+        
+        
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        for i, node in enumerate(self.helper(root), 1):
-            if i == k:
-                return node.val
-            
-    def helper(self, root) -> Generator:
-        if root:
-            yield from self.helper(root.left)
-            yield root
-            yield from self.helper(root.right)
-    
+        self.helper(root, k)
+        return self.result
