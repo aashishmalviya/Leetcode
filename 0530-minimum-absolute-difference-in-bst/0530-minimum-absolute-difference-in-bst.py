d3d2404de59422ba73db4ad3e84bd184b9594eb3
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-23 08:09:51 +0530

    Time: 51 ms (85.73%), Space: 19.3 MB (12.25%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -5,12 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.min_diff = 1e9
+        self.prev_node = None
+            
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
         
-        def minabs_generator(root):
-            if root:
-                yield from minabs_generator(root.right)
-                yield root.val
-                yield from minabs_generator(root.left)
-                
-        return min(starmap(operator.sub, pairwise(minabs_generator(root))))
+        def inorder(node):
+            
+            if not node:
+                return
+            
+            inorder(node.left)
+            
+            if self.prev_node:
+                self.min_diff = min(self.min_diff, node.val - self.prev_node.val)
+            
+            self.prev_node = node
+            
+            inorder(node.right)
+        
+        
+        inorder(root)
+        return int(self.min_diff)
