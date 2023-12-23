fe227ae1b4b4df91c3001d1b3ed20518f770ce2d
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-23 11:02:41 +0530

    Time: 80 ms (99.78%), Space: 19.2 MB (48.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,32 +1,37 @@
 from collections import defaultdict
 
 class Solution:
+
+    NOT_VISITED = 0
+    VISIT_IN_PROGRESS = 1
+    VISIT_COMPLETED = 2
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         dependencies = defaultdict(list)
 
         for course, course_preq in prerequisites:
             dependencies[course].append(course_preq)
 
-        visiting = set()
+        visit_status = [self.NOT_VISITED] * numCourses
 
-        def cycle_present(course):
-            if not dependencies[course]:
-                return False
-
-            if course in visiting:
+        def cycle_present(course) -> bool:
+            if visit_status[course] == self.VISIT_IN_PROGRESS:
                 return True
 
-            visiting.add(course)
+            if visit_status[course] == self.VISIT_COMPLETED:
+                return False
+
+            visit_status[course] = self.VISIT_IN_PROGRESS
 
             for dependency in dependencies[course]:
                 if cycle_present(dependency):
                     return True
 
-            dependencies[course] = []
+            visit_status[course] = self.VISIT_COMPLETED
             return False
 
-        for course in range(numCourses):
-            if cycle_present(course):
+        for current_course in range(numCourses):
+            if cycle_present(current_course):
                 return False
 
         return True
