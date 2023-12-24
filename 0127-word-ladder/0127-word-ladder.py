1a3c2cb8ff3aad433c9a0f101c1cd3c4b337dace
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-24 07:57:50 +0530

    Time: 110 ms (96.92%), Space: 20.4 MB (29.65%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        
+        if endWord not in wordList:
+            return 0
+        
+        neighbors = defaultdict(list)
+        
+        wordList.append(beginWord)
+        
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j+1:]
+                neighbors[pattern].append(word)
+                
+        visited = set()
+        visited.add(beginWord)
+        
+        q = deque()
+        q.append(beginWord)
+        
+        result = 1
+        
+        while q:
+            
+            qLen = len(q)
+            
+            for i in range(qLen):
+                
+                word = q.popleft()
+                
+                if word == endWord:
+                    return result
+                
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1: ]
+                    
+                    for current_neighbor in neighbors[pattern]:
+                        if current_neighbor not in visited:
+                            visited.add(current_neighbor)
+                            q.append(current_neighbor)
+            
+            result += 1
+        
+        return 0
