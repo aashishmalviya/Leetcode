a59efdc42db77ba479ea21710edc8b539359cde5
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-25 08:35:46 +0530

    Time: 725 ms (97.13%), Space: 65.6 MB (15.05%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.temp_list = []
+        self.result = []
+        
+    def combine(self, n: int, k: int) -> List[List[int]]:       
+        def combination_populator(start: int, n: int, k: int):
+            if len(self.temp_list) == k:
+                self.result.append(self.temp_list[:])
+                # print(self.result)
+                return
+
+            for i in range(start, n+1):
+                self.temp_list.append(i)
+                combination_populator(i+1, n, k)
+                del self.temp_list[-1]
+
+        combination_populator(1, n, k)
+        # print(self.temp_list)
+        # print(self.result)
+        return self.result
