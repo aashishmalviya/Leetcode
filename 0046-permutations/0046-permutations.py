7bf039bccd2cf0ecc9a34469c4701ab55c4dd032
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-25 09:01:20 +0530

    Time: 35 ms (96.71%), Space: 17.6 MB (5.24%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permutation_helper(self, nums, final_result, current_res):
+        if not nums:
+            final_result.append(current_res)
+            return
+
+        for i in range(len(nums)):
+            self.permutation_helper(nums[:i] + nums[i+1:], final_result, current_res + [nums[i]])
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        final_result = []
+        current_res = []
+        self.permutation_helper(nums, final_result, current_res)
+        return final_result
