22700c6b023c14cbf6a268c3c5ea6577b46261c5
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-25 07:46:13 +0530

    Time: 34 ms (83.50%), Space: 17.5 MB (5.00%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -5,7 +5,7 @@ class Solution:
         if not digits:
             return []
 
-        digit_map ={'2' : 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7':'pqrs', '8': 'tuv', '9': 'xwyz'}
+        digit_map = {'2' : 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7':'pqrs', '8': 'tuv', '9': 'xwyz'}
 
         result = []
 
