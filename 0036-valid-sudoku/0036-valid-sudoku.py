3ef28ecc03b1016fe585e1572446a45981c5053b
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2023-12-30 09:30:33 +0530

    Time: 147 ms (5.24%), Space: 17.2 MB (15.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/valid-sudoku/discuss/3277043/Beats-96.78-oror-Short-7-line-Python-solution-(with-detailed-explanation)
+# res += [(i, element), (element, j), (i // 3, j // 3, element)]
+# Pay attention to types (int, str), (str, int), (int, int, str) and what occurences they describe.
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         result = []
