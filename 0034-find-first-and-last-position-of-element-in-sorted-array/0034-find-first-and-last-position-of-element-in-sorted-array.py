28fdb7a701b24759b6c7ac29d852338ae8607e87
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-02 05:56:02 +0530

    Time: 66 ms (99.50%), Space: 18.6 MB (11.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,7 +1,35 @@
+# class Solution:
+#     def searchRange(self, nums: List[int], target: int) -> List[int]:
+#         left = bisect_left(nums, target)
+#         right = bisect_right(nums, target)        
+#         return [left, right-1] if left != right else [-1, -1]
+
+# https://www.youtube.com/watch?v=4sQL7R5ySUU
 class Solution:
+    def bin_search(self, nums, target, left_aligned):
+        left, right = 0, len(nums)-1
+
+        index = -1
+
+        while left <= right:
+            mid = (left + right) >> 1
+
+            if nums[mid] < target:
+                left = mid + 1
+
+            elif nums[mid] > target:
+                right = mid - 1
+
+            else:
+                index = mid
+                if left_aligned:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+        return index
+
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        left = bisect_left(nums, target)
-        right = bisect_right(nums, target)
-        
-        return [left, right-1] if left != right else [-1, -1]
+        left = self.bin_search(nums, target, True)
+        right = self.bin_search(nums, target, False)
+        return [left, right]
