7b9fb5ef78d0fdc0068f798bafcd41b35f473ca0
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-03 04:40:52 +0530

    Time: 587 ms (21.31%), Space: 30.9 MB (11.38%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -11,3 +11,40 @@ class Solution:
                 heapq.heappush(min_heap, num)
 
         return min_heap[0]
+
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         def swap(lst, a, b):
+#             lst[a], lst[b] = lst[b], lst[a]
+
+#         n = len(nums)
+
+#         def partition(start, end):
+#             pivot_element = nums[end]
+
+#             pivot_index = start - 1
+
+#             for i in range(start, end):
+#                 if nums[i] < pivot_element:
+#                     pivot_index += 1
+#                     swap(nums, i, pivot_index)
+
+#             swap(nums, pivot_index + 1, end)
+#             return pivot_index + 1
+
+#         def quick_select(start, end):
+#             if start < end:
+#                 pivot_index = partition(start, end)
+
+#                 if pivot_index == n-k:
+#                     return nums[pivot_index]
+
+#                 elif pivot_index > (n-k):
+#                     return quick_select(start, pivot_index - 1)
+
+#                 else:
+#                     return quick_select(pivot_index + 1, end)
+
+#             return nums[start]
+
+#         return quick_select(0, n-1)
