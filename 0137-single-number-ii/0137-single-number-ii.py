f08fa820d85b1ec3cec1c44103e212a294c459a0
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-04 04:51:25 +0530

    Time: 68 ms (48.44%), Space: 19.5 MB (10.73%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+	def singleNumber(self, nums: list[int]) -> int:
+		if not nums:
+		    return 0
+		   
+		ones, twos = 0, 0
+		for num in nums:
+			ones = (ones ^ num) & (~twos)
+			twos = (twos ^ num) & (~ones)
+		return ones
+
+	# def testSol(self):
+	# 	assert self.singleNumber([2, 2, 3, 2]) == 3
+	# 	assert self.singleNumber([]) == 0
+	# 	assert self.singleNumber(None) == 0
+	# 	print("All ok")
+
+# test = Solution()
+# test.testSol()
+# print(test.singleNumber([2, 2, 3, 2]))
