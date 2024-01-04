e293004e68bbffab04f233352cbf3e7ba601e92c
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-04 05:32:20 +0530

    Time: 44 ms (95.04%), Space: 17.3 MB (21.37%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/bitwise-and-of-numbers-range/discuss/593317/Simple-3-line-Java-solution-faster-than-100
+# https://leetcode.com/problems/bitwise-and-of-numbers-range/discuss/3220550/201%3A-Solution-with-step-by-step-explanation
+
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         while left < right:
