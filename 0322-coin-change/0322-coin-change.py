4b82c6f71f80e9d52e8c076bbcd67c1acc2c4200
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-05 09:25:14 +0530

    Time: 1876 ms (9.10%), Space: 420.2 MB (5.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,34 @@
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         dp = [0] + [amount+1] * (amount)
+        
+#         coins.sort()
+        
+#         for currentAmount in range(1,amount+1):
+#             for coin in coins:
+#                 if coin <= currentAmount:
+#                     dp[currentAmount] = min(dp[currentAmount], dp[currentAmount - coin] + 1)
+#                 else:
+#                     break
+        
+#         return dp[amount] if dp[amount] != amount+1 else -1
+
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [0] + [amount+1] * (amount)
+        dp = {}
+        def dfs_helper(index, amount):
+            if index < 0 or amount < 0:
+                return 1e9
+            
+            if (index, amount) in dp:
+                return dp[(index, amount)]
+            
+            if amount == 0:
+                return 0
+            
+            dp[(index, amount)] = min(1 + dfs_helper(index, amount - coins[index]), dfs_helper(index-1, amount))
+            return dp[(index, amount)]
         
-        coins.sort()
-        
-        for currentAmount in range(1,amount+1):
-            for coin in coins:
-                if coin <= currentAmount:
-                    dp[currentAmount] = min(dp[currentAmount], dp[currentAmount - coin] + 1)
-                else:
-                    break
-        
-        return dp[amount] if dp[amount] != amount+1 else -1
+        ans = dfs_helper(len(coins)-1, amount)
+        return ans if ans!= 1e9 else -1
