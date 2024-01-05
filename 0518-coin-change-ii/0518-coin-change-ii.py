8830f7e653aaced843f4c25805c138b885994a85
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-05 09:25:16 +0530

    Time: 116 ms (85.86%), Space: 38.8 MB (30.84%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,11 +1,31 @@
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         dp = [1] + [0] * amount
+        
+#         #coins.sort()
+        
+#         for currentCoin in coins:
+#             for currentAmount in range(currentCoin, amount+1):
+#                 dp[currentAmount] += dp[currentAmount - currentCoin]
+                
+#         return dp[amount]
+
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [1] + [0] * amount
+        dp = {}
         
-        #coins.sort()
+        def dfs_helper(index, amount):
+            if index < 0 or amount < 0:
+                return 0
+            
+            if (index, amount) in dp:
+                return dp[(index, amount)]
+            
+            if amount == 0:
+                return 1
+            
+            dp[(index, amount)] = dfs_helper(index, amount - coins[index]) + dfs_helper(index - 1, amount)
+            return dp[(index, amount)]
         
-        for currentCoin in coins:
-            for currentAmount in range(currentCoin, amount+1):
-                dp[currentAmount] += dp[currentAmount - currentCoin]
-                
-        return dp[amount]
+        return dfs_helper(len(coins)-1, amount)
