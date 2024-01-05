f843b85e6c9ebebcd22059955827ebed1c29d027
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-05 09:49:40 +0530

    Time: 31 ms (99.03%), Space: 17.5 MB (9.77%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,14 +1,42 @@
+# class Solution:
+#     def combinationSum4(self, nums: List[int], target: int) -> int:
+#         dp = [1] + [0] * (target+1)
+        
+#         nums.sort()
+        
+#         for currentTarget in range(1, target+1):
+#             for currentNum in nums:
+#                 if currentNum <= currentTarget:
+#                     dp[currentTarget] = (dp[currentTarget] + dp[currentTarget - currentNum])
+#                 else:
+#                     break
+                    
+#         return dp[target]
+
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [1] + [0] * (target+1)
         
-        nums.sort()
-        
-        for currentTarget in range(1, target+1):
-            for currentNum in nums:
-                if currentNum <= currentTarget:
-                    dp[currentTarget] = (dp[currentTarget] + dp[currentTarget - currentNum])
-                else:
+        dp = collections.defaultdict(int)
+        dp[0] = 1
+        def dfs_helper(current_target) -> int:
+            if current_target < 0:
+                return 0
+            
+            if current_target in dp:
+                return dp[current_target]
+            
+            if current_target == 0:
+                return 1
+            
+            res = 0
+            for n in nums:
+                if current_target < n:
                     break
-                    
-        return dp[target]
+                res += dfs_helper(current_target - n)
+            dp[current_target] = res
+            return res
+        
+        #print(dp.items())
+        #return dp[target]
+        nums.sort()
+        return dfs_helper(target)
