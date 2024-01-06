8c885f035268087971de12f2e2a531725f59f600
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-06 12:55:21 +0530

    Time: 420 ms (64.79%), Space: 136.3 MB (11.39%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,8 +1,10 @@
+from functools import lru_cache
+
 class Solution:
+    @lru_cache(None)
     def isMatch(self, s: str, p: str) -> bool:
         
-        dp = [[-1] * (len(p)+1) for _ in range(len(s) + 1)]
-
+        @cache
         def solver(i, j) -> bool:
             if i<0 and j<0: return True
 
@@ -14,17 +16,10 @@ class Solution:
                         return False
                 return True
 
-            if dp[i][j] != -1:
-                return dp[i][j]
-
             if s[i] == p[j] or p[j] == '?':
-                dp[i][j] = solver(i-1, j-1)
-                return dp[i][j]
-
+                return solver(i-1, j-1)
             elif p[j] == '*':
-                dp[i][j] = solver(i, j-1) or solver(i-1, j)
-                return dp[i][j]
-
+                return solver(i, j-1) or solver(i-1, j)
             else:
                 return False
 
