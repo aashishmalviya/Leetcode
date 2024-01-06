1a0bef412f809d6204ad0cc45e229c28a4def9e7
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-06 12:50:31 +0530

    Time: 273 ms (73.76%), Space: 26.6 MB (31.64%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,7 +1,8 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        @cache
+        dp = [[-1] * (len(p)+1) for _ in range(len(s) + 1)]
+
         def solver(i, j) -> bool:
             if i<0 and j<0: return True
 
@@ -13,10 +14,17 @@ class Solution:
                         return False
                 return True
 
+            if dp[i][j] != -1:
+                return dp[i][j]
+
             if s[i] == p[j] or p[j] == '?':
-                return solver(i-1, j-1)
+                dp[i][j] = solver(i-1, j-1)
+                return dp[i][j]
+
             elif p[j] == '*':
-                return solver(i, j-1) or solver(i-1, j)
+                dp[i][j] = solver(i, j-1) or solver(i-1, j)
+                return dp[i][j]
+
             else:
                 return False
 
