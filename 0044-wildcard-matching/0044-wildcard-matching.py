e23ea738eb2e97360401004030015d1485a16f54
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-06 13:00:58 +0530

    Time: 290 ms (73.27%), Space: 26.6 MB (31.64%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -4,28 +4,28 @@ class Solution:
         dp = [[-1] * (len(p)+1) for _ in range(len(s) + 1)]
 
         def solver(i, j) -> bool:
-            if i<0 and j<0: return True
+            if i==0 and j==0: return True
 
-            if i>=0 and j<0: return False
+            if i>0 and j==0: return False
 
-            if i<0 and j>=0:
-                for k in range(0, j+1):
-                    if p[k] != '*':
+            if i==0 and j>0:
+                for k in range(1, j+1):
+                    if p[k-1] != '*':
                         return False
                 return True
 
             if dp[i][j] != -1:
                 return dp[i][j]
 
-            if s[i] == p[j] or p[j] == '?':
+            if s[i-1] == p[j-1] or p[j-1] == '?':
                 dp[i][j] = solver(i-1, j-1)
                 return dp[i][j]
 
-            elif p[j] == '*':
+            elif p[j-1] == '*':
                 dp[i][j] = solver(i, j-1) or solver(i-1, j)
                 return dp[i][j]
 
             else:
                 return False
 
-        return solver(len(s)-1, len(p)-1)
+        return solver(len(s), len(p))
