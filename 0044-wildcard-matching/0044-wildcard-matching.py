f1411c5a82cfae18f83e9bbe0086c848b4f147d8
Aashish Malviya <148682943+aashishmalviya@users.noreply.github.com>
2024-01-06 13:42:06 +0530

    Time: 464 ms (58.17%), Space: 26.2 MB (35.32%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,31 +1,62 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        dp = [[-1] * (len(p)+1) for _ in range(len(s) + 1)]
+        #dp = [[-1] * (len(p)+1) for _ in range(len(s) + 1)]
 
-        def solver(i, j) -> bool:
-            if i==0 and j==0: return True
+        def solverTab(s: str, p: str) -> bool:
+            dp = [[False for j in range (len(p)+1)] for _ in range(len(s) + 1)]
+            
+            dp[0][0] = True
 
-            if i>0 and j==0: return False
-
-            if i==0 and j>0:
+            for j in range(1, len(p)+1):
+                flag = True
                 for k in range(1, j+1):
                     if p[k-1] != '*':
-                        return False
-                return True
+                        flag = False
+                        break
+                dp[0][j] = flag
+                
+            # for j in range(1,m+1):
+            # if(p[j-1] == '*' ): dp[0][j] = dp[0][j-1];
 
-            if dp[i][j] != -1:
-                return dp[i][j]
+            for i in range(1, len(s) + 1):
+                for j in range(1, len(p) + 1):
 
-            if s[i-1] == p[j-1] or p[j-1] == '?':
-                dp[i][j] = solver(i-1, j-1)
-                return dp[i][j]
+                    if s[i-1] == p[j-1] or p[j-1] == '?':
+                        dp[i][j] = dp[i-1][j-1]
+ 
+                    elif p[j-1] == '*':
+                        dp[i][j] = dp[i][j-1] or dp[i-1][j]
 
-            elif p[j-1] == '*':
-                dp[i][j] = solver(i, j-1) or solver(i-1, j)
-                return dp[i][j]
 
-            else:
-                return False
+            #print(dp)
+            return dp[len(s)][len(p)]
 
-        return solver(len(s), len(p))
+
+
+#         def solver(i, j) -> bool:
+#             if i==0 and j==0: return True
+
+#             if i>0 and j==0: return False
+
+#             if i==0 and j>0:
+#                 for k in range(1, j+1):
+#                     if p[k-1] != '*':
+#                         return False
+#                 return True
+
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
+
+#             if s[i-1] == p[j-1] or p[j-1] == '?':
+#                 dp[i][j] = solver(i-1, j-1)
+#                 return dp[i][j]
+
+#             elif p[j-1] == '*':
+#                 dp[i][j] = solver(i, j-1) or solver(i-1, j)
+#                 return dp[i][j]
+
+#             else:
+#                 return False
+
+        return solverTab(s, p)
